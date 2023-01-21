d0821d5f4cc199beb3e8660c62df399f07d1fc69
Jason Lynch <jason@calindora.com>
2023-01-20 20:34:45 -0800

    Add a skeleton actix-web project.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("0.0.0.0", 3000))?
+        .run()
+        .await
 }
