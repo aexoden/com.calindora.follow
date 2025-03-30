bd67caa61358d9555339aedb1048a79cfa0e1c25
Jason Lynch <jason@aexoden.com>
2025-03-29 18:53:05 -0700

    Remove React template boilerplate, establish linting and fix issues.
    Signed-off-by: Jason Lynch <jason@aexoden.com>

---
+++ frontend/eslint.config.ts
@@ -0,0 +1,52 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-plugin-prettier/recommended";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import stylistic from "@stylistic/eslint-plugin";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    stylistic.configs.customize({
+        braceStyle: "1tbs",
+        indent: 4,
+        semi: true,
+    }),
+    { ignores: ["dist"] },
+    {
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.strictTypeChecked,
+            ...tseslint.configs.stylisticTypeChecked,
+            {
+                languageOptions: {
+                    parserOptions: {
+                        projectService: true,
+                        tsconfigRootDir: import.meta.dirname,
+                    },
+                },
+            },
+            prettier,
+        ],
+        files: ["**/*.{js,ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            "@stylistic": stylistic,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "@stylistic/arrow-parens": ["error", "always"],
+            "@stylistic/linebreak-style": ["error", "unix"],
+            "@stylistic/no-extra-parens": "error",
+            "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
+            "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+            "sort-keys": ["error", "asc", { natural: true }],
+        },
+    },
+);
