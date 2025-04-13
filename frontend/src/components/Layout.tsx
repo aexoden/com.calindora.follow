77e37ac7a210c0037e0d97d286e3fc3d881138dd
Jason Lynch <jason@aexoden.com>
2025-04-13 09:23:28 -0700

    Attempt to fix mobile layout by using dvh units for the height.
    Signed-off-by: Jason Lynch <jason@aexoden.com>

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function Layout() {
     return (
-        <div className="flex h-screen flex-col bg-gray-50">
+        <div className="flex h-dvh flex-col bg-gray-50">
             <main className="flex flex-grow flex-col">
                 <Outlet />
             </main>
